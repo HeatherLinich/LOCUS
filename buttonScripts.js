416d60d4d774d0ac489fd69f1f909bf481ac9bc3
Heather Mayer <hlinich@luc.edu>
2015-03-12 16:27:00 -0500

    initial commit

---
+++ buttonScripts.js
@@ -0,0 +1,15 @@
+function mainPageClick()
+ {
+    window.location="MainLOCUS.html"; 
+ }
+   
+   
+function backClick()
+ {
+    window.location="AvailableEnglishClasses.html"; 
+ }
+ 
+ function enrollClick()
+ {
+    window.alert("Success! Enrolled in English 102");
+ }
