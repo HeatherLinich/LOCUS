537305cc30fd3378ae011ccc9c954a03aef4ac75
Heather Mayer <hlinich@luc.edu>
2015-03-27 13:44:32 -0500

    new updates to files

---
+++ js/buttonScripts.js
@@ -0,0 +1,19 @@
+function mainPageClick()
+ {
+    window.location="MainLOCUS.html"; 
+ }
+   
+   
+function backClick()
+ {
+    window.location="AvailableEnglishClasses.html"; 
+ }
+ 
+
+function schedulesFunction() {
+    var x = document.getElementById("Schedules").value;
+    if(x==1)
+    {
+        window.location="Spring2015.html"; 
+    }
+}
